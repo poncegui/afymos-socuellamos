0ab3dabe3ad7e49357c0f9eb648a40f30a210cfa
Poncegui <marta.ponceg@gmail.com>
2024-07-08 07:46:51 +0200

    rename files transparencia download

--- src/views/transparencia/components/ProjectsIntro.jsx
+++ src/views/transparencia/components/ProjectsIntro.jsx
@@ -3,6 +3,7 @@ import TitleSection from "../../../components/TitleSection";
 import doc1 from "./assets/docs_transparencia/memoria-diputacion-2023.pdf";
 import doc10 from "./assets/docs_transparencia/informe-auditoria-viviendas-apoyo-2023.pdf";
 import doc11 from "./assets/docs_transparencia/memoria-afymos-joven-2023.pdf";
+import doc12 from "./assets/docs_transparencia/cuentas-anuales-2023.pdf";
 import doc2 from "./assets/docs_transparencia/informe-auditoria-centro-dia-2024.pdf";
 import doc3 from "./assets/docs_transparencia/memoria-centro-dia-2023.pdf";
 import doc4 from "./assets/docs_transparencia/plan-estrategico-2023-2024.pdf";
@@ -16,17 +17,18 @@ import styled from "styled-components";
 const DescargaTransparencia = () => {
 
   const links = [
-    { id: 1, url: doc1, name: 'Memoria Diputación' },
-    { id: 2, url: doc2, name: 'Informe auditoría viviendas' },
-    { id: 3, url: doc3, name: 'Memoria Afymos' },
-    { id: 4, url: doc4, name: 'Informe auditoría Centro Día' },
-    { id: 5, url: doc5, name: 'Memoria Centro Día' },
-    { id: 6, url: doc6, name: 'Plan de Voluntariado' },
-    { id: 7, url: doc7, name: 'Memoria Vivienda I' },
-    { id: 8, url: doc8, name: 'Memoria Vivienda II' },
-    { id: 9, url: doc9, name: 'Memoria Servicio capacitación' },
-    { id: 10, url: doc10, name: 'Auditoría Viviendas' },
-    { id: 11, url: doc11, name: 'Memoria Afymos Joven' },
+    { id: 1, url: doc1, name: 'Memoria ayuda equipamiento: Diputación Ciudad Real' },
+    { id: 2, url: doc2, name: 'Informe auditoría de cuentas: Vivienda con apoyo' },
+    { id: 3, url: doc3, name: 'Memoria: Programa fomento autonomía personal' },
+    { id: 4, url: doc4, name: 'Plan Estratégico: "En ruta"' },
+    { id: 5, url: doc5, name: 'Memoria evaluativa: Centro Día' },
+    { id: 6, url: doc6, name: 'Plan de Voluntariado: 2022-2024' },
+    { id: 7, url: doc7, name: 'Memoria: Programa de atención resindencial I' },
+    { id: 8, url: doc8, name: 'Memoria: Programa de atención resindencial II' },
+    { id: 9, url: doc9, name: 'Memoria: Programa de atención sociolaboral' },
+    { id: 10, url: doc10, name: 'Informe: Cuentas justificativa subvenciones' },
+    { id: 11, url: doc11, name: 'Memoria: Afymos Joven 2023' },
+    { id: 12, url: doc12, name: 'Cuentas Anuales 2023' },
   ];
 
   return (
@@ -81,11 +83,11 @@ export const ButtonContainer = styled.div`
   display: grid;
   width: 100%;
   height: 100%;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-columns: 1fr 1fr 1fr ;
   grid-template-rows: 1fr;
   margin: auto;
   grid-gap: 5% 5%;
-  margin: 5% 20%;
+  margin: 5% 10%;
   justify-content: center;
 
   @media (max-width: 768px) {
@@ -98,6 +100,11 @@ export const ButtonContainer = styled.div`
     margin: 5%;
     margin-bottom: 15%;
     width: 90%;
+
+    @media (max-width: 440px) {
+      grid-template-columns: 1fr ;
+    grid-template-rows: 1fr ;
+  }
   }
 `;
 
@@ -118,10 +125,13 @@ export const Icons = styled.div`
   text-align: center;
   align-items: center;
   border-radius: 10px;
-  width: 150px;
-  height: 100px;
+  width: 350px;
+  height: 150px;
   box-shadow: 5px 5px 10px #c6c5c5, -5px -5px 10px #ffffff;
   text-decoration:none;
+  background-color: "#c6b1c9";
+
+  
 
 
   :hover {
@@ -134,21 +144,19 @@ export const Icons = styled.div`
     padding: 0 0.3rem;
     color:  #071c2f;
   }
-
     cursor: pointer;
 
-
-  @media (max-width: 368px) {
-    margin: 5%;
-    margin-bottom: 15%;
-    gap: 0.6rem;
+  @media (max-width: 440px) {
+    margin:3%;
     border-radius: 10px;
     width: 80%;
     box-shadow: 5px 5px 10px #949494, -5px -5px 10px #ffffff;
     border: solid 1px #071c2f;
+    width: 350px;
+  height: 150px;
 
     h3 {
-      font-size: 0.5rem;
+      font-size: 1.5rem;
       padding: 0 0.3rem;
       color:  #071c2f;
     }
