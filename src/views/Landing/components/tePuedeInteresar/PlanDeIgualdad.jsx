0b8d6bcf595758d83d7f19d9be019ccf04ea379b
Poncegui <marta.ponceg@gmail.com>
2025-02-05 18:45:04 +0100

    add build new

---
+++ src/views/Landing/components/tePuedeInteresar/PlanDeIgualdad.jsx
@@ -0,0 +1,165 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import { cardsServiceData } from "./services/CardsData";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import styled from "styled-components";
+
+const SeccionContainer = styled.section`
+  width: 100%;
+  padding-top: 5%;
+`;
+
+const Titulo = styled.h2`
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 20px;
+  position: relative;
+  display: inline-block;
+  font-size: 24px;
+  text-decoration: none;
+
+  @media (max-width: 900px) {
+    margin-top: 50px;
+    font-size: 20px;
+    margin-bottom: 40px;
+  }
+`;
+
+const CircleIcon = styled.div`
+  position: absolute;
+  top: 50%;
+  left: -30px;
+  transform: translateY(-50%);
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: #224464;
+
+  @media (max-width: 900px) {
+    width: 16px;
+    height: 16px;
+  }
+`;
+
+const ContenedorCards = styled.div`
+  display: flex;
+  margin-top: 20px;
+  align-items: start;
+
+  @media (max-width: 900px) {
+    margin-top: 3%;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 20px;
+    justify-content: center;
+    align-items: center;
+    width: 60%;
+    margin: auto;
+  }
+`;
+
+const Card = styled.div`
+  flex: 0 1 calc(33.333% - 16px);
+  max-width: 260px;
+  height: 400px;
+  margin-bottom: 16px;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: ${(props) => props.color};
+  margin-right: 16px;
+  position: relative;
+
+  @media (max-width: 900px) {
+    flex: 0 1 calc(50% - 16px);
+    margin-right: 8px;
+    margin-bottom: 8px;
+    height: 350px;
+  }
+`;
+
+const TituloCard = styled.h3`
+  color: ${(props) => (props.color === "#224464" ? "#c6b1c9" : "#224464")};
+  font-size: 16px;
+  text-align: center;
+  position: absolute;
+  left: 50%;
+  bottom: 50px;
+  transform: translateX(-50%);
+  width: 100%;
+
+  @media (max-width: 768px) {
+    font-size: 14px;
+  }
+`;
+
+const Image = styled.img`
+  width: 100px;
+  max-height: 70%;
+  margin-bottom: 10px;
+`;
+
+const LinkButton = styled.a`
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  position: absolute;
+  bottom: 10px;
+  left: 10px;
+  font-size: 20px;
+  transition: color 0.3s, transform 0.3s;
+  text-decoration: none;
+  color: inherit;
+
+  @media (max-width: 768px) {
+    font-size: 24px;
+  }
+
+  &:hover {
+    color: #071c2f;
+    transform: rotate(90deg);
+  }
+`;
+
+const StyledFontAwesomeIcon = styled(FontAwesomeIcon)`
+  font-size: 30px;
+
+  @media (max-width: 768px) {
+    font-size: 26px;
+  }
+`;
+
+const CardsInterestInformation = () => {
+  return (
+    <SeccionContainer>
+      <Titulo>
+        <CircleIcon />
+        Te puede interesar...
+      </Titulo>
+      <ContenedorCards>
+        {cardsServiceData.map((card, index) => (
+          <Card key={index} color={index === 0 ? "#81b71a" : card.color}>
+            <Image src={card.image} alt={card.alt} />
+            <LinkButton
+              aria-label="ir a la ruta seleccionada"
+              href={card.url}
+              target="_blank"
+              download={card.download}
+            >
+              <StyledFontAwesomeIcon
+                icon={faPlus}
+                style={{
+                  color: card.color === "#224464" ? "#c6b1c9" : "#071c2f",
+                }}
+              />
+            </LinkButton>
+            <TituloCard color={card.color}>{card.title}</TituloCard>
+          </Card>
+        ))}
+      </ContenedorCards>
+    </SeccionContainer>
+  );
+};
+
+export default CardsInterestInformation;
--- src/views/Landing/components/tePuedeInteresar/services/CardsData.js
+++ src/views/Landing/components/tePuedeInteresar/services/CardsData.js
@@ -2,6 +2,7 @@ import charlas from "../assets/icons/comment-regular.svg";
 import events from "../assets/icons/face-smile-wink-solid.svg";
 import graduation from "../assets/icons/graduation.svg";
 import pdf from './crea_2025.pdf'
+import planIgualdad from '../assets/pdf/planIgualdad.pdf'
 // import podcast from "../assets/icons/spotify.svg";
 import podcast2 from "../../../assets/images-cover/afymos-show.jpg";
 
@@ -51,3 +52,46 @@ export const cardsServiceData = [
 
 
 
+export const cardsIgualdadData = [
+  {
+    key: 1,
+    title: "Compromiso con la igualdad",
+    description:
+      "Alumnos y trabajadores nos cuentan su experiencia en el NUEVO CREA 2025.",
+    image: podcast2,
+    alt: "!Afymos estrena podcast!",
+    url: planIgualdad,
+    color: "#f5f5f5",
+  },
+  {
+    key: 2,
+    title: "NUEVO CREA 2025",
+    description:
+      "Afymos es entidad promotora del Proyecto de Formación y Empleo en el seno de la empresa NUEVO CREA",
+    image: graduation,
+    alt: "proyecto-crea",
+    url: pdf,
+    color: "#224464",
+  },
+  {
+    key: 3,
+    title: "CHARLAS EDUCATIVAS",
+    description:
+      "LA EDUCACIÓN INCLUSIVA con la colaboración de los docentes de IES Fernando de Mena  y Centro Concertado Virgen de Loreto.",
+    image: charlas,
+    alt: "Charlas profesionales",
+    url: "https://fb.watch/r896b1s2MT/",
+    color: "#c6b1c9",
+  },
+  {
+    key: 4,
+    title: "ACUERDO ADEVI y FEDA",
+    description:
+      "FEDA y ADEVI firman un acuerdo por el que facilitarán contratos de formación a los participantes de Asprona",
+    image: events,
+    alt: "acuerdo con Feda y Adevi",
+    url:"https://villarrobledonoticias.com/noticias/2024-06-feda-y-adevi-firman-un-acuerdo-por-el-que-facilitaran-contratos-de-formacion-a-los-participantes-de-asprona-en-la-formacion-para-personas-con-discapacidad-de-la-asociacion-afymos/",
+    color: "#f5f5f5",
+
+  },
+];
